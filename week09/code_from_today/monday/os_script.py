09f294d7ba925f0853ab759cfaca5b36708f40f1
clbokea <clbo@kea.dk>
2020-02-24 10:34:27 +0100

    after os

---
+++ week09/code_from_today/monday/os_script.py
@@ -0,0 +1,15 @@
+# os_script.py
+
+
+import os
+
+os.mkdir('TEST')
+
+os.chdir('TEST')
+
+f = open('ttt.py', 'w')
+f.write('import mod')
+
+
+
+
--- week09/code_from_today/monday/script.py
+++ week09/code_from_today/monday/script.py
@@ -5,9 +5,7 @@
 from mod import func
 
 
-print(func())
-
-
+print(func()) 
 # print(mod.l)
 # print(mod.func())
 
