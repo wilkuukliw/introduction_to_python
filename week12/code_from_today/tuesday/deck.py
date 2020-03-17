241463adc781f04603d1c053002a894207315894
Anna <anna.maria.wilczek@gmail.com>
2020-03-17 13:17:17 +0100

    changes to exercises

--- week12/code_from_today/tuesday/deck.py
+++ week12/code_from_today/tuesday/deck.py
@@ -1,23 +1,23 @@
 #deck.py
 
 class Deck:
-    def __init__(self):
+    def __init__(self):                 #   >> d = Deck()
         self.cards = ['A', 'K', 4, 7]   # list of cards that are included in deck
 
     def __getitem__(self,key):
-        return self.cards[key]
+        return self.cards[key]              # >> d[1] 
 
     def __len__(self):
-        return len(self.cards)
+        return len(self.cards)              # >> len(c)
 
-    def __add__(self,other):
-        return self.cards + other.cards
+    def __add__(self,other):                #  >> c = Deck()
+        return self.cards + other.cards     #  >> d + c 
+ 
+    def __repr__(self):                     # >> d    or top level representation repr(d)
+        return f'{self.__dict__}'           # >> {'cards': ['A', 'K', 4, 7]}      
 
-    def __repr__(self):
-        return str(self.cards)
-
-    def __str__ (self):
-        return str(self.cards)
+    def __str__ (self):                     # informal representation
+        return str(self.cards)              # >> str(d)    >> "['A', 'K', 4, 7]"
 
     def __setitem__ (self, key, val):
         self.cards[key] = val
