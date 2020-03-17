3e7d9638513d2712ed1d87076f8bda66f2c4e029
clbokea <clbo@kea.dk>
2020-03-17 10:50:56 +0100

    deck solution

--- week12/code_from_today/tuesday/deck.py
+++ week12/code_from_today/tuesday/deck.py
@@ -1,5 +1,15 @@
 # deck.py
+ 
+"""
+implement the:
 
+        __len__         method
+        __add__         method
+        __repr__        method
+        __str__         method
+        __setitem__     method
+        __delitem__     method
+"""
 
 class Deck:
     def __init__(self):
@@ -8,3 +18,40 @@ class Deck:
 
     def __getitem__(self, key):
         return self.cards[key]
+
+    def __setitem__(self, key, val):
+        self.cards[key] = val
+
+
+    def __delitem__(self, key):
+        del(self.cards[key])
+
+    def __len__(self):
+        return len(self.cards)
+
+
+    def __add__(self, other):
+        return self.cards + other.cards
+
+    def __repr__(self):
+        return str(self.cards)
+
+    def __str__(self):
+        return str(self.cards)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
