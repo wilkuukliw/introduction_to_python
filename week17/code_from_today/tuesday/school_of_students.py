436352b664c298e900d88f728c78c69f5bf36a71
clbokea <clbo@kea.dk>
2020-04-21 12:59:29 +0200

    end

---
+++ week17/code_from_today/tuesday/school_of_students.py
@@ -0,0 +1,55 @@
+# school_of_students.py
+
+import random
+import time
+
+# Timer decorator
+def timer(func):
+    def wrapper(*args):
+        start = time.time()
+        val = func(*args)
+        end = (time.time()) - start
+        print(f'Time elepsed: {end}')
+        return val
+    return wrapper
+
+
+# The exercise:
+
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+@timer
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        result.append(person)
+    # return result
+
+@timer
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        yield person
+
+
+
+
+
+
+
+
+
+
+
+
+
